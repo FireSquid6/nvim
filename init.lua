d667250ab87838bdeb341a575af1862eaf47fc69
firesquid <jdeiss06@gmail.com>
2023-09-23 18:14:29 -0500

    add floating terminal support

--- init.lua
+++ init.lua
@@ -23,6 +23,20 @@ require('lazy').setup({
     'wakatime/vim-wakatime',
   },
 
+  -- terminal
+  {
+    "rebelot/terminal.nvim",
+    config = function()
+      require("terminal").setup(
+        {
+            layout = { open_cmd = "float" },
+            cmd = { vim.o.shell },
+            autoclose = false,
+        }
+      )
+    end
+  },
+
   -- Github Copilot
   'Lucklyric/copilot.vim',
 
@@ -258,6 +272,22 @@ vim.keymap.set('n', '<leader>fs', '<cmd>w<cr>', { desc = 'Save This File' })
 vim.keymap.set('n', '<leader>fS', '<cmd>wa<cr>', { desc = 'Save all Files' })
 vim.keymap.set('n', '<leader>fq', '<cmd>wq<cr>', { desc = 'Quit and Save this File'})
 vim.keymap.set('n', '<leader>fQ', '<cmd>wqa<cr>', { desc = 'Quit and Save all Files' })
+
+-- terminal
+local term_map = require("terminal.mappings")
+vim.keymap.set({ "n", "x" }, "<leader>ts", term_map.operator_send, { expr = true })
+vim.keymap.set("n", "<leader>to", term_map.toggle)
+vim.keymap.set("n", "<leader>tO", term_map.toggle({ open_cmd = "enew" }))
+vim.keymap.set("n", "<leader>tr", term_map.run)
+vim.keymap.set("n", "<leader>tR", term_map.run(nil, { layout = { open_cmd = "enew" } }))
+vim.keymap.set("n", "<leader>tk", term_map.kill)
+vim.keymap.set("n", "<leader>t]", term_map.cycle_next)
+vim.keymap.set("n", "<leader>t[", term_map.cycle_prev)
+vim.keymap.set("n", "<leader>tl", term_map.move({ open_cmd = "belowright vnew" }))
+vim.keymap.set("n", "<leader>tL", term_map.move({ open_cmd = "botright vnew" }))
+vim.keymap.set("n", "<leader>th", term_map.move({ open_cmd = "belowright new" }))
+vim.keymap.set("n", "<leader>tH", term_map.move({ open_cmd = "botright new" }))
+vim.keymap.set("n", "<leader>tf", term_map.move({ open_cmd = "float" }))
 
 -- SET OPTIONS
 vim.opt.smartcase = true
